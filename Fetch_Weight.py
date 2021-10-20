0301b3f4ddb17c1d969670cfe8b335cb9f8dab5e
Dominic Paul Isaacs <43357287+DomIsaacs@users.noreply.github.com>
2021-10-20 14:54:09 -0400

    working version of fetch weight.py

--- Fetch_Weight.py
+++ Fetch_Weight.py
@@ -1,40 +1,43 @@
 from socket import *
 import time
+import xlwt
+from xlwt import Workbook
+
+path = 'C:/Users/domis/projects/weight_sensor_test_folder/xlwt_example.xls'
+
+
+
+# Workbook is created
+wb = Workbook()
+sheet1 = wb.add_sheet('Sheet 1')
+
+# set global variable flag
+address =('10.90.125.56', 1900) #define server IP and port, this may be a little tricky, each mega is assigned a different IP (but they can all talk to the same port, at least one at a time)
+#address = ('10.90.125.43',1900)
+client_socket=socket(AF_INET, SOCK_DGRAM) #Set up the 'Socket' which is just a dumb name for a communication chnnel
+client_socket.settimeout(2) #Only wait 2 second for a response
  
-address=('10.90.125.50', 1900) #define server IP and port
-client_socket=socket(AF_INET, SOCK_DGRAM) #Set up the Socket
-client_socket.settimeout(1) #Only wait 1 second for a response
- 
-while(1):
- 
-    data = "Temperature" #Set data request to Temperature
- 
-    client_socket.sendto(str.encode(data), address) #Send the data request
- 
-    try:
- 
-        rec_data, addr = client_socket.recvfrom(2048) #Read response from arduino
-        temp = float(rec_data) #Convert string rec_data to float temp
-        print("Rat Weight: ", temp, "grams.")# Print the result
- 
-    except:
-        pass
- 
-    time.sleep(2) #delay before sending next command
- 
-    data = "Pressure" #Set data request to Pressure
- 
-    client_socket.sendto(str.encode(data), address) #Send the data request
+while True:
+    
+    #we ask for temprature
+    data_request = "req" #Set data request to weight
+    client_socket.sendto(str.encode(data_request), address) #Send the data request
  
     try:
  
         rec_data, addr = client_socket.recvfrom(2048) #Read response from arduino
-        Pressure = float(rec_data) #Convert string rec_data to float temp
-        print("The Measured Pressure is ", Pressure, " Pa.") # Print the result
+        weight = float(rec_data) #Convert string rec_data to float temp
+        print("weight:",weight, "g.")# Print the result
+
  
     except:
         pass
- 
+    
     time.sleep(2) #delay before sending next command
- 
+
+    
     print("")
+
+sheet1.write(1, 0, "wei")
+sheet1.write(2, 0, weight)
+wb.save(path)
